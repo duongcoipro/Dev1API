255c446a496f48c9cb4b6a2f7bfe0c3fcadeee1f
duongcoipro <chichariduong@gmail.com>
2025-05-07 14:13:36 +0700

    dev

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import glob
 import json
+from zlapi import ZaloAPI
+from zlapi.models import *
+
+imei = "a7da7a49-9f7e-4fb7-8173-154af9d2af66-f1f6b29a6cc1f79a0fea05b885aa33d0"
+cookies = {"_ga_NCLZ4KG7XE":"GS1.1.1729050963.1.1.1729051022.0.0.0","_gcl_au":"1.1.1760086498.1741684853","_ga_NVN38N77J3":"GS1.2.1741685714.2.0.1741685714.0.0.0","_ga_E63JS7SPBL":"GS1.1.1741685713.2.0.1741685718.55.0.0","_ga_1J0YGQPT22":"GS1.1.1741686996.2.1.1741687341.55.0.0","zoaw_sek":"xmje.891775287.2.wvkWGa5M71nsFpifGLOS745M71n1LXmSGB9x42bM71m","zoaw_type":"0","__zi":"3000.SSZzejyD7iu_cVEzsr0LpYAPvhoKKa7GR9V-_yX0IvuWa_ssXqL9coQQiBQ5J0sP9PEzlO913PC.1","__zi-legacy":"3000.SSZzejyD7iu_cVEzsr0LpYAPvhoKKa7GR9V-_yX0IvuWa_ssXqL9coQQiBQ5J0sP9PEzlO913PC.1","_ga":"GA1.2.627150477.1720074500","_gid":"GA1.2.1191676216.1746593671","_zlang":"vn","googtrans":"/auto/vi","_ga_907M127EPP":"GS2.1.s1746597880$o3$g0$t1746597880$j60$l0$h0","_ga_RYD7END4JE":"GS2.2.s1746598038$o6$g1$t1746598080$j18$l0$h0","zpsid":"CRyt.434311158.3.XTy1h9vPmnDeLY0Aabd34-igiIUjRV0YhsZsApMu5FYKgLYSdrKv2VPPmnC","zpw_sek":"EvWr.434311158.a0.d7wkTHTaIZBXQwahDcHACcn6BrarNcXpK14715jN7M8bFGnKNomoPM4FALL6MNSGQi-hvVnkkgcOPKR83GnACW","app.event.zalo.me":"5352338974368243918"}
+bot = ZaloAPI("0876089909", "Diamond99!!", imei=imei, cookies=cookies)
+msg = Message(text="The system starts automatically at: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+#bot.sendMessage(msg, 8046989271300889247, ThreadType.GROUP)
+
 
 app = FastAPI()
 app.add_middleware(
@@ -48,4 +57,4 @@ def list_uploads():
             image_file = info.get("Hình ảnh", "")
             info["image_url"] = f"/images/{image_file}"
             results.append(info)
-    return results
+    return results 
